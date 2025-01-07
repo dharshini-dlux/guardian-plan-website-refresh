15bb747808f0570e4f634e336e929b4b9094b2af
nithish-dlux <163503283+nithish-dlux@users.noreply.github.com>
2025-01-07 16:28:51 +0530

    config for new colors implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      seaweed: {
+        DEFAULT: "#008C95",
+        900: "#002A2D",
+        800: "#00464B",
+        700: "#005459",
+        600: "#007077",
+        500: "#008C95",
+        400: "#33A3AA",
+        300: "#66BABF",
+        200: "#99D1D5",
+        100: "#CCE8EA",
+        50: "#F2F9FA",
+      },
+      lightSeaweed: {
+        DEFAULT: "#84C4C0",
+      },
+    },
+    fontFamily: {
+      display: "Archivo, medium",
+      body: "Helvetica, sans-serif",
+    },
     extend: {},
   },
   plugins: [],
